084c68e5a19aadd6e22cb15d4ae51f68aee364a6
igorcampos-dev <igorccampos8@gmail.com>
2025-07-29 19:25:38 -0300

    feat(spring-keycloak-example): add base

---
+++ spring-keycloak-example/src/main/java/com/io/example/SpringKeycloakExampleApplication.java
@@ -0,0 +1,13 @@
+package com.io.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeycloakExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKeycloakExampleApplication.class, args);
+	}
+
+}
