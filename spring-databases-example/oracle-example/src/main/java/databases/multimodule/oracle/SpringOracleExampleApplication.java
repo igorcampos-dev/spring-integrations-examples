c99269597645c5c5c08fb50d206bf384c8064131
igorcampos-dev <igorccampos8@gmail.com>
2025-10-30 22:36:33 -0300

    feat(spring-databases-example): move spring-oracle-example module

---
+++ spring-databases-example/oracle-example/src/main/java/databases/multimodule/oracle/SpringOracleExampleApplication.java
@@ -0,0 +1,13 @@
+package databases.multimodule.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOracleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOracleExampleApplication.class, args);
+	}
+
+}
