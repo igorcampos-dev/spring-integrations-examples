57747c546e6af9b724966ebbd3f3b530024121b2
igorcampos-dev <igorccampos8@gmail.com>
2025-10-31 21:16:58 -0300

    feat(spring-databases-example): move files and modules

---
+++ spring-mysql-example/src/test/java/com/io/example/SpringMysqlExampleApplicationTest.java
@@ -0,0 +1,16 @@
+package com.io.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class SpringMysqlExampleApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
---
+++ spring-oracle-example/src/main/java/com/io/example/SpringOracleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.io.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOracleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOracleExampleApplication.class, args);
+	}
+
+}
